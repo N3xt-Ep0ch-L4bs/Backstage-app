77ef7b25a9ab45c9125d36d4bbd5ebeafe5abfd2
Ashley-code396 <ashleymwende24@gmail.com>
2025-11-19 17:47:23 +0300

    Set up providers

---
+++ src/networkConfig.ts
@@ -0,0 +1,16 @@
+// Copyright (c), Mysten Labs, Inc.
+// SPDX-License-Identifier: Apache-2.0
+import { getFullnodeUrl } from '@mysten/sui/client';
+import { TESTNET_PACKAGE_ID } from './constants';
+import { createNetworkConfig } from '@mysten/dapp-kit';
+
+const { networkConfig, useNetworkVariable, useNetworkVariables } = createNetworkConfig({
+  testnet: {
+    url: getFullnodeUrl('testnet'),
+    variables: {
+      packageId: TESTNET_PACKAGE_ID,
+    },
+  },
+});
+
+export { useNetworkVariable, useNetworkVariables, networkConfig };
